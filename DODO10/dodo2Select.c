5578a8a817e27bbf35617f38a25bf0431647f8be
Albert <berman.albert@gmail.com>
2019-03-20 09:00:06 +0100

    first

---
+++ DODO10/dodo2Select.c
@@ -0,0 +1,68 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  dodo2Select.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  18.03.2019 06:08:03
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h> 
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+        int i;
+        for(i = 0; i<argc; i++) {
+                printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        }
+        printf("\n");
+        return 0;
+}
+
+int main(int argc, char* argv[]) {
+        sqlite3 *db;
+        char *zErrMsg = 0;
+        int rc;
+        char *sql;
+        const char* data = "Callback function called";
+
+        /* Open database */
+        rc = sqlite3_open("dodo10.db", &db);
+
+        if( rc ) {
+                fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+                return(0);
+        } else {
+                fprintf(stdout, "Opened database successfully\n");
+        }
+
+
+        /*--------------- Create SQ statement ---------------------*/
+        sql = "SELECT * from COMPANY";
+
+        /* Execute SQL statement */
+        rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+
+        if( rc != SQLITE_OK ) {
+                fprintf(stderr, "SQL error: %s\n", zErrMsg);
+                sqlite3_free(zErrMsg);
+        } else {
+                fprintf(stdout, "Operation done successfully\n");
+        }
+
+        /*---------------------------------------------------------*/
+
+        sqlite3_close(db);
+        return 0;
+}
---
+++ DODO10/pythonDodo10.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 red <red@red-Swift-SF113-31>
+#
+# Distributed under terms of the MIT license.
+"""
+
+"""
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+
+def select_all_tasks(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM COMPANY")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def select_task_by_priority(conn, priority):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM COMPANY WHERE ID=?", (priority, ))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def main():
+    database = '/home/red/git/aTest/dotFiles/nVim/pdbA/mysql/DODO10/dodo10.db'
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        print("2. Query all tasks")
+        select_all_tasks(conn)
+
+        print("1. Query task by ID:")
+        select_task_by_priority(conn, 1)
+
+
+if __name__ == '__main__':
+    main()
