72f59b17e31ccca6dab1d1e16d6787c238b6d093
Albert <berman.albert@gmail.com>
2019-03-21 06:26:10 +0100

    Git-Quick-Run

---
+++ kDot/4GitRun.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+# 4Run.sh
+# Copyright (C) 2019 red <red@red-Swift-SF113-31>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+git log --oneline 
+
+git diff --stat HEAD~5
+
+
